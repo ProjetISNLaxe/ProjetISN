3d63d30114dc98918d19c8f0b7d7a4c028a93be0
ProjetISNLaxe <35565165+ProjetISNLaxe@users.noreply.github.com>
2018-05-01 17:08:42 +0200

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,136 @@
+﻿import pygame as pg
+from random import randint
+from Options import *
+vec = pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.standing_frames[0]
+        self.rect = self.image.get_rect()
+        self.pos = vec(10, HAUTEUR/2)
+        self.vit = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def load_images(self) :
+        self.standing_frames = [pg.image.load('img/R1.png').convert_alpha()]
+
+        self.walk_frames_r = [pg.image.load('img/R1.png'),
+                              pg.image.load('img/R2.png'),
+                              pg.image.load('img/R3.png')]
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r :
+            frame.convert_alpha()
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+
+        self.jump_frame_r = self.walk_frames_r[2]
+        self.jump_frame_l = self.walk_frames_l[2]
+
+    def jump(self):
+        # on saute seulemnt si l'on est au sol ous ur une plateforme
+        self.rect.x+=1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -=1
+        if hits :
+            self.vit.y = -PLAYER_JUMP
+
+    def update(self):
+        self.animate()
+        self.acc = vec(0,PLAYER_GRAV)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_RIGHT]:
+            self.acc.x = PLAYER_ACC
+
+        # on applique les frottements du sol
+        self.acc.x += self.vit.x * PLAYER_FRICTION
+        #équations du mouvement
+        self.vit += self.acc
+        if abs(self.vit.x) < 0.1 :
+            self.vit.x = 0
+        self.pos += self.vit + 0.5 * self.acc
+
+        if self.pos.x <= 20 :
+            self.pos.x = 20
+        if self.pos.x >= 780 :
+            self.pos.x = 780
+        self.rect.midbottom = self.pos
+
+    def animate(self):
+        now = pg.time.get_ticks()
+
+        if self.vit.x != 0 :
+            self.walking = True
+        else :
+            self.walking = False
+
+        if self.vit.y != 0 :
+            self.jumping = True
+        else :
+            self.jumping = False
+
+        if self.jumping :
+        # animation personnage pendant le saut
+            bottom = self.rect.bottom
+            if self.vit.x >= 0 :
+                self.image = self.jump_frame_r
+            else :
+                self.image = self.jump_frame_l
+            self.rect = self.image.get_rect()
+            self.rect.bottom = bottom
+
+        if self.walking and not self.jumping :
+            # animation personnage pendant la course
+            if now - self.last_update > 180 :
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frames_l)
+                bottom = self.rect.bottom
+                if self.vit.x > 0 :
+                    self.image = self.walk_frames_r[self.current_frame]
+                else :
+                    self.image = self.walk_frames_l[self.current_frame]
+                self.rect =self.image.get_rect()
+                self.rect.bottom = bottom
+
+        # animation du personnage lorsqu'il n'y a pas de mouvement
+        if not self.jumping and not self.walking :
+             if now - self.last_update > 350 :
+                 self.last_update = now
+                 self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                 bottom = self.rect.bottom
+                 self.image = self.standing_frames[self.current_frame]
+                 self.rect = self.image.get_rect()
+                 self.rect.bottom = bottom
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.create_plat()
+        self.image = self.plat_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def create_plat(self) :
+        n = int(randint(1, 10))
+        rect_img = []
+        self.platform = [pg.image.load('img/plateforme1.png')]
+        for i in range (1, n+1) :
+            self.platform.append(pg.image.load('img/plateforme2.png'))
+        self.platform.append(pg.image.load('img/plateforme3.png'))
+        self.plat_image = pg.Surface((40+n*20, 20))
+        for i in range (0, len(self.platform)) :
+            self.platform[i] = self.platform[i].convert_alpha()
+            rect_img.append(self.platform[i].get_rect())
+            rect_img[i][0] = rect_img[i-1][0]+ rect_img[i-1][2]
+            if i == 0 :
+                rect_img[i][0] = 0
+            self.plat_image.blit(self.platform[i], (rect_img[i][0], 0))
---
+++ Principal.py
@@ -0,0 +1,131 @@
+﻿import pygame as pg
+import random
+from Options import *
+from Classes import *
+from os import path
+
+class Jeu :
+    def __init__(self) :
+        # initialisation de la fenêtre, etc
+        pg.init()
+        self.fenetre = pg.display.set_mode((LARGEUR,HAUTEUR))
+        pg.display.set_caption(TITRE)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
+
+    def new(self) :
+        # commencer une nouvelle partie
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST :
+            p = Platform(self, *plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        self.run()
+
+    def run(self):
+        # boucle du jeu
+        self.playing = True
+        while self.playing == True :
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.display()
+
+    def update(self):
+        # boucle du jeu mise à jour
+        self.all_sprites.update()
+        # on vérifie si le joueur touche une plateforme (uniquement en descendant)
+        if self.player.vit.y > 0 :
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top+1
+                self.player.vit.y = 0
+        #si le joueur arrive au 2/3 de la largeur de l'écran
+        if self.player.rect.x >= LARGEUR/3 :
+            self.player.pos.x -= max(abs(self.player.vit.x),2)
+            for plat in self.platforms :
+                plat.rect.x -= max(abs(self.player.vit.x),2)
+                if plat.rect.x <= 0 :
+                    plat.kill()
+
+        #créer de nouvelles plateformes
+        while len(self.platforms) < 5 :
+            p = Platform(self, random.randrange(LARGEUR, LARGEUR+30),
+                         random.randrange(100, HAUTEUR-30))
+            self.platforms.add(p)
+            self.all_sprites.add(p)
+
+        # lorsque l'on meurt
+        if self.player.rect.bottom > HAUTEUR :
+            self.playing = False
+
+    def events(self) :
+        # actions / événements
+        for event in pg.event.get() :
+            if event.type == pg.QUIT :
+                if self.playing == True :
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN :
+                if event.key == pg.K_SPACE :
+                    self.player.jump()
+
+    def display(self) :
+        # boucle d'affichage du jeu
+        self.fenetre.fill(COULEUR_FOND)
+        self.all_sprites.draw(self.fenetre)
+        self.fenetre.blit(self.player.image, self.player.rect)
+        self.affiche_text(str(self.score), 30, BLANC, LARGEUR-20, 20)
+        # après affichage de tous les éléments, on rafraîchit l'écran
+        pg.display.flip()
+
+    def affiche_text(self, text, size, color, x, y) :
+        #affiche le nombre d'ennemis tués lors de la phase 1
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.fenetre.blit(text_surface, text_rect)
+
+    def start_screen(self):
+        # écran d'accueil
+        self.fenetre.fill(COULEUR_FOND)
+        self.affiche_text('RUNNER', 48, JAUNE, LARGEUR/2, HAUTEUR/4)
+        self.affiche_text("FLECHES pour BOUGER, ESPACE pour SAUTER", 22 , JAUNE, LARGEUR/2, HAUTEUR/2)
+        self.affiche_text("APPUYEZ sur une TOUCHE pour JOUER", 22 , JAUNE, LARGEUR/2, HAUTEUR*(3/4))
+        pg.display.flip()
+        self.wait_for_key()
+
+    def game_over_screen(self):
+        # écran lorsque l'on perd
+        if self.running == False :
+            return
+        self.fenetre.fill(COULEUR_FOND)
+        self.affiche_text('GAME OVER', 48, ROUGE, LARGEUR/2, HAUTEUR/4)
+        self.affiche_text("APPUYEZ sur une TOUCHE pour REJOUER", 22 , ROUGE, LARGEUR/2, HAUTEUR/2)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting :
+            self.clock.tick(FPS)
+            for event in pg.event.get() :
+                if event.type == pg.QUIT :
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP :
+                    waiting = False
+
+g = Jeu()
+g.start_screen()
+while g.running :
+    g.new()
+    g.game_over_screen()
+
+pg.quit()
